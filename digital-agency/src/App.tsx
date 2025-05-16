717b83360e04cbe473ad85373b217f261f9719d1
Sanjana1363 <sanjanasatish69@gmail.com>
2025-05-16 18:57:57 +0530

    Update app to single-page scroll with internal anchors, removed routing and contact page

--- digital-agency/src/App.tsx
+++ digital-agency/src/App.tsx
@@ -1,35 +1,37 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import Contact from './pages/Contact';
-import './index.css'
+import Hero from './components/Hero';
+import Services from './components/Services';
+import Portfolio from './components/Portfolio';
+import './index.css';
+import Testimonials from './components/Testimonial';
 
 const App: React.FC = () => {
   return (
-    <><header className="header">
-  <div className="container nav-container">
-    <h1 className="logo">DigitalWave</h1>
-    <nav className="navbar">
-      <a href="/home">Home</a>
-      <a href="/services">Services</a>
-      <a href="/portfolio">Portfolio</a>
-    
-    </nav>
-  </div>
-</header>
-
+    <>
+      <header className="header">
+        <div className="container nav-container">
+          <h1 className="logo">DigitalWave</h1>
+          <nav className="navbar">
+            <a href="#home">Home</a>
+            <a href="#services">Services</a>
+            <a href="#portfolio">Portfolio</a>
+          </nav>
+        </div>
+      </header>
 
       <main>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/contact" element={<Contact />} />
-        </Routes>
+        {/* Add id="home" here to scroll on "Home" */}
+        <section id="home">
+          <Hero />
+        </section>
+        <Services />
+        <Portfolio />
+        <Testimonials/>
       </main>
 
       <footer className="footer">
         <div className="container">
           &copy; 2025 DigitalWave Technology & Services LLP
-
         </div>
       </footer>
     </>
